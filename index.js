4bd232b6834f93abbb558a716d23d65c28b83eba
User <hanifbbz@gmail.com>
2017-05-31 16:17:08 +0200

    only payment apis in this repo

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 'use strict';
 
-const agreement = require('./lib/api/agreement');
-const asset = require('./lib/api/asset');
-const auth = require('./lib/api/auth');
-const campaign = require('./lib/api/campaign');
-const connection = require('./lib/api/connection');
-const dataobject = require('./lib/api/dataobject');
-const eid = require('./lib/api/eid');
-const kpi = require('./lib/api/kpi');
-const paylink = require('./lib/api/paylink');
-const platform = require('./lib/api/platform');
-const product = require('./lib/api/product');
-const subscription = require('./lib/api/subscription');
-const token = require('./lib/api/token');
-const user = require('./lib/api/user');
-const voucher = require('./lib/api/voucher');
+const campaign = require('./lib/campaign');
+const paylink = require('./lib/paylink');
+const product = require('./lib/product');
+const subscription = require('./lib/subscription');
+const voucher = require('./lib/voucher');
 
 module.exports = {
-    agreement, asset, auth, campaign, connection, dataobject, eid, kpi, paylink, platform, product,
-    subscription, token, user, voucher
+    campaign, paylink, product, subscription, voucher
 };
